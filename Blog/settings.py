0031ec2ec82802d94e75277c0b1c44d33f807d0c
Pavan Ram Gangadhar Kumar <gangathecoder425@gmail.com>
2023-11-21 17:10:54 +0530

    Changed Debug In Setting

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5oz8v1bnrkt0d40!)r%c(k9bt$c%v0w4fna39ucojv$2asn@r6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
