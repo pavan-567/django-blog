54aea20f742958ef10b661c1a0f953ffe2221717
Pavan Ram Gangadhar Kumar <gangathecoder425@gmail.com>
2023-11-21 17:23:09 +0530

    Some More Changes

--- Blog/settings.py
+++ Blog/settings.py
@@ -161,10 +161,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'Auth.User'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = 'media/'
--- Blog/urls.py
+++ Blog/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('Profile/', include('Profile.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = 'Blog.views.handler_404'
